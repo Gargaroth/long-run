d6b636df46e8d7354507ce0a8bacba21cecbb392
Gargaroth <82537441+Gargaroth@users.noreply.github.com>
2021-04-15 20:39:00 +0300

    Add files via upload

---
+++ Projects/Арифметика/src/Арифметика.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Арифметика {
+    public static void main(String[] args) {
+
+        Scanner x = new Scanner(System.in);
+
+        System.out.println("Введите первое число: ");
+        double a = x.nextDouble();
+        System.out.println("Введите второе число: ");
+        double b = x.nextDouble();
+
+        Scanner z = new Scanner(System.in);
+        System.out.println("Что вы хотели бы следать: Сложить, Вычесть, Умножить, Разделить? ");
+        String q = z.next();
+
+        switch (q) {
+
+            case "Сложить": {
+                System.out.println(a + b);
+                break;
+            }
+            case "Вычесть": {
+                System.out.println(a - b);
+                break;
+            }
+            case "Умножить": {
+                System.out.println(a * b);
+                break;
+            }
+            case "Разделить": {
+                System.out.println(a / b);
+                break;
+            }
+            }
+        }
+    }
+
---
+++ Projects/блфблф/src/блфблф.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class блфблф {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+
+        System.out.println("Введите число: ");
+        int a = x.nextInt();
+
+        System.out.println("Введите число: ");
+        int b = x.nextInt();
+
+        System.out.println(a+b);
+    }
+
+}
