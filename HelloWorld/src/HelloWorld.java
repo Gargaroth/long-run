48130f927ef505283f7dec7787f5694bbfff5b2c
Gargaroth <82537441+Gargaroth@users.noreply.github.com>
2021-04-15 13:53:40 +0300

    Add files via upload

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Введите значиения a и b");
+        int a;
+        int b;
+        int x = a+b;
+        String name;
+                System.out.println(x);
+            }
+}
