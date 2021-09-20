aa17dfa0c18d9a11114a1dcf196affba0b857c99
Gargaroth <82537441+Gargaroth@users.noreply.github.com>
2021-09-20 13:17:16 +0300

    Add files via upload

--- Projects/clock/src/clock.java
+++ Projects/clock/src/clock.java
@@ -1,14 +1,39 @@
-public class clock {
-    public static void main(String[] args) {
-        for (; ; ) {
-            for (int sec = 1; sec <= 60; sec++) {
-                System.out.println(sec);
-                if (sec == 60)
-                for (int hour = 1; hour<=24; hour++){
-                    System.out.println(hour);
+class clock {
 
+    public static void main(String[] args) {
+
+
+        for (int s = 0; s < 43200; s++) {
+
+            double h = s / 120d;
+            double m = (s / 10d) % 360;
+
+            double angle = Math.abs(h - m);
+            angle = round(angle);
+            if (angle == 180) {
+                s=format(s);
+                System.out.println(s);
+                s+=3600;
             }
         }
     }
+
+    public static double round(double value) {
+        value *= 10;
+        value = Math.round(value);
+        return value/10d;
+
+    }
+
+    public static double format (double  s) {
+        double hour = s/3600;
+        s = s%3600;
+        double min = s/60;
+        double sec = s%60;
+        return System.out.println(hour+":"+min+":"+sec);
+
+    }
+
 }
-}
+
+
