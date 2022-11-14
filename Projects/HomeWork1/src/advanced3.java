0164cc6807184faa7e649390d13bcaff1ff7d55a
Тимофей Вихров <82537441+Gargaroth@users.noreply.github.com>
2022-11-14 21:08:52 +0300

    Add files via upload

--- Projects/HomeWork1/src/advanced3.java
+++ Projects/HomeWork1/src/advanced3.java
@@ -1,10 +1,14 @@
 public class advanced3 {
     public static void main(String[] args) {
+
+        /* блин, вижу что задача решается просто, но вот решать я ее стал не правильно и не пойму с помощью чего ее надо решать,
+        скорее всего цикл, но не уверен, буду рад подсказке какую конструкцию использовать и переделаю, уверен моих знаний достаточно для ее решения  */
+
         boolean hasFuel = true;
-        boolean hasElectricsProblem = false;
+        boolean hasElectricsProblem = true;
         boolean hasMotorProblems = false;
-        boolean hasTransmissionProblem = false;
-        boolean hasWheelProblem = false;
+        boolean hasTransmissionProblem = true;
+        boolean hasWheelProblem = true;
 
         int Fuel = 1000;
         int Motor = 10000;
@@ -16,34 +20,43 @@ public class advanced3 {
 
         if (hasMotorProblems) {
             overallPrice = Motor;
-        }s
-        else if (hasElectricsProblem) {
+            if (hasMotorProblems && hasElectricsProblem) {
+                overallPrice = (Motor + Electrics) * 0.9;
+            } else if (hasMotorProblems && hasWheelProblem) {
+                overallPrice = (Motor + Wheel) * 0.9;
+            } else if (hasMotorProblems && hasTransmissionProblem) {
+                overallPrice = (Motor + Transmission) * 0.9;
+            }
+            if (hasTransmissionProblem && (hasMotorProblems || hasElectricsProblem)) {
+                overallPrice = overallPrice * 0.8;
+            }
+        } else if (hasElectricsProblem) {
             overallPrice = Electrics;
-        }
-        else if (hasTransmissionProblem) {
+
+            if (hasElectricsProblem && hasWheelProblem) {
+                overallPrice = (Electrics + Wheel) * 0.9;
+            } else if (hasElectricsProblem && hasTransmissionProblem) {
+                overallPrice = (Electrics + Transmission) * 0.9;
+            }
+            if (hasTransmissionProblem && (hasMotorProblems || hasElectricsProblem)) {
+                overallPrice = overallPrice * 0.8;
+            }
+        } else if (hasTransmissionProblem) {
             overallPrice = Transmission;
-        }
-        else if (hasWheelProblem){
+            if (hasTransmissionProblem && hasElectricsProblem) {
+                overallPrice = (Transmission + Electrics) * 0.9;
+            } else if (hasTransmissionProblem && hasWheelProblem) {
+                overallPrice = (Transmission + Wheel) * 0.9;
+            }
+            if (hasTransmissionProblem && (hasMotorProblems || hasElectricsProblem)) {
+                overallPrice = overallPrice * 0.8;
+            }
+
+        } else if (hasWheelProblem) {
             overallPrice = Wheel;
-        }
-        else if (hasMotorProblems && hasElectricsProblem) {
-            overallPrice = (Motor+Electrics)*0.9;
-        }
-        else if (hasMotorProblems && hasWheelProblem) {
-            overallPrice = (Motor + Wheel) * 0.9;
-        }
-        else if (hasMotorProblems && hasTransmissionProblem) {
-            overallPrice = (Motor + Transmission) * 0.9;
-        }
-        else if (hasElectricsProblem && hasWheelProblem) {
-            overallPrice = (Electrics + Transmission) * 0.9;
-        }
-        else if (hasTransmissionProblem && hasWheelProblem) {
-            overallPrice = (Transmission + Wheel) * 0.9;
-        }
-        else {
-            overallPrice=Fuel;
+        } else {
+            overallPrice = Fuel;
         }
         System.out.println(overallPrice);
-    }
-}
+    }}
+// блин, вижу что легкая задача, но вот решать я ее стал не правильно и не пойму с помощью чего ее надо решать,
